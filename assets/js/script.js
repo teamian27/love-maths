d63b5d03ac0dc0485e794cee06d1597b94ece2ee
teamian27 <>
2023-10-25 20:02:31 +0000

    add division code and finish project

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,7 +37,9 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
-    } else{
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
+    } else {
        alert(`Unkown game type: ${gameType}`);
        throw `Unknow game type: ${gameType}. Aborting!`;
     }
@@ -74,6 +76,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if  (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if  (operator === "/") {
+        return [operand1 / operand2, "division"]; 
     } else {
         alert(`Unimplemnted operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -114,5 +118,15 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+    operand1 = operand1 * operand2;
+
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById('operator').textContent = "/";
 
 }
